ab8b4b01f394a85351c276f4286886c315e2a1aa
Paskab012 <pascalkabikampan@gmail.com>
2022-09-18 18:22:03 +0200

    Setup the project using tailwindcss configurations

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        brightRed: '#F53838',
+        brightWhiteBackground: '#F6F6F6',
+        darkBlue: '#0B132A',
+        darkGrayishBlue: '#4F5665',
+        borderColors: '#DDDDDD',
+        titleColor: '#0B132A',
+        textColor: '#4F5665',
+      },
+
+      dropShadow: {
+        '3xl': '0 35px 35px #F53838',
+        '4xl': ['0 35px 35px #F53838', '0 45px 65px #F53836'],
+      },
+    },
+  },
+  plugins: [],
+};
